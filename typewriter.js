8248985ee30c50b78c53a732e677b8af83bf1262
Smiti <mishra.smiti06@gmail.com>
2021-09-21 14:40:48 -0400

    updated the file with timeout & processstoutwrite

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let t = 0;
+for (const char of sentence) {
+  t = t + 50;
+  setTimeout(() => {
+    process.stdout.write(char);
+   
+  },t)  
+  
+}
+setTimeout(() => {process.stdout.write('\n')}, t)
